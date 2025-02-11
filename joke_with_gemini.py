4bc1955d4167b47f499bdc2b0ef3ed420e484925
kefasogabi <kefasogabi@gmail.com>
2025-02-11 23:46:42 +0100

    gemini llm

---
+++ joke_with_gemini.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+import google.generativeai
+from IPython.display import Markdown, display, update_display
+
+
+load_dotenv(override=True)
+google_api_key = os.getenv('GOOGLE_API_KEY')
+
+google.generativeai.configure()
+
+system_message = "You are an assistant that is great at telling jokes"
+user_prompt = "Tell a light-hearted joke for an audience of Data Scientists"
+
+prompts = [
+    {"role": "system", "content": system_message},
+    {"role": "user", "content": user_prompt}
+  ]
+
+gemini = google.generativeai.GenerativeModel(
+    model_name='gemini-2.0-flash-exp',
+    system_instruction=system_message
+)
+response = gemini.generate_content(user_prompt)
+print(response.text)
